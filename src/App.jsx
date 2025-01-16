9962a5a9688b911afb397aeb1e85653218d46d48
undertanker86 <taduong.work@gmail.com>
2025-01-16 18:53:27 +0700

    config theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Button from '@mui/material/Button'
 import HomeIcon from '@mui/icons-material/Home'
-
+import Typography from '@mui/material/Typography'
 
 
 function App() {
@@ -8,6 +8,7 @@ function App() {
   return (
     <>
       <h1>Hello Word</h1>
+      <Typography variant="body2" color="text.secondary">H1</Typography>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
       <Button variant="outlined">Outlined</Button>
